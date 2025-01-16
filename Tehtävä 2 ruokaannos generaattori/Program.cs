c5788ded230036ab272b5e184916c77a5a584559
Eel1s0 <s2300903@edu.bc.fi>
2025-01-16 12:35:37 +0200

    tehtävä 2 lisätty

--- Tehtävä 2 ruokaannos generaattori/Program.cs
+++ Tehtävä 2 ruokaannos generaattori/Program.cs
@@ -26,20 +26,77 @@
 
         class Ateria
         {
-            public PääraakaAine Pääraaka { get; set; }
+            public PääraakaAine PääraakaAine { get; set; }
             public Lisuke Lisuke { get; set; }
             public Kastike Kastike { get; set; }
 
             public override string ToString()
             {
-                return $"{Pääraaka} ja {Lisuke} {Kastike}-kastikkeella";
+                return $"{PääraakaAine} ja {Lisuke} {Kastike}-kastikkeella";
             }
+        }
 
+        static void Main(string[] args)
+        {
+            List<Ateria> ateriat = new List<Ateria>();
 
-            static void Main(string[] args)
+            for (int i = 0; i < 3; i++)
             {
+                Console.WriteLine($"\nLuo ruoka-annos {i + 1}:");
+
+                Console.WriteLine("Valitse pääraaka-aine");
+                ValitseJaTulostavaihtoehdot<PääraakaAine>();
+                PääraakaAine pääraaka = (PääraakaAine)Valitsevaihtoehto(typeof(PääraakaAine));
+
+                Console.WriteLine("Valitse lisuke");
+                ValitseJaTulostavaihtoehdot<Lisuke>();
+                Lisuke lisuke = (Lisuke)Valitsevaihtoehto(typeof(Lisuke));
+
+                Console.WriteLine("Valitse kastike");
+                ValitseJaTulostavaihtoehdot<Kastike>();
+                Kastike kastike = (Kastike)Valitsevaihtoehto(typeof(Kastike));
+
+                Ateria ateria = new Ateria
+                {
+                    PääraakaAine = pääraaka,
+                    Lisuke = lisuke,
+                    Kastike = kastike
+                };
+
+                ateriat.Add(ateria);
+                Console.WriteLine($"Ruoka-annos tallennettu: {ateria}");
 
             }
+
+            Console.WriteLine("\nLoit seuraavat ruoka-annokset:");
+            foreach (var ateria in ateriat)
+            {
+                Console.WriteLine(ateria);
+            }
+
+        }
+
+        static void ValitseJaTulostavaihtoehdot<T>() where T : Enum
+        {
+            string[] vaihtoehdot = Enum.GetNames(typeof(T));
+            for (int i = 0; i < vaihtoehdot.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}: {vaihtoehdot[i]}");
+            }
+        }
+
+        static int Valitsevaihtoehto(Type enumType)
+        {
+            while (true)
+            {
+                string vastaus = Console.ReadLine();
+                if (int.TryParse(vastaus, out int valinta) && valinta >= 1 && valinta <= Enum.GetNames(enumType).Length)
+                {
+                    return valinta - 1;
+                }
+                Console.WriteLine("Virheellinen valinta, yritä uudelleen");
+            }
+
 
         }
     }
